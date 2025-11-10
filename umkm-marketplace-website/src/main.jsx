cf1b006af43e8c60710f812649e5ea6de1dee53f
PutuRifkiDy <puturifki56@gmail.com>
2025-11-11 00:08:47 +0800

    added: Router Setup

--- umkm-marketplace-website/src/main.jsx
+++ umkm-marketplace-website/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Beranda from "./pages/Beranda";
+import Blog from "./pages/Blog";
+import DetailProduct from "./pages/DetailProduct";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import RegisterShop from "./pages/RegisterShop";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Beranda />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+  {
+    path: "/detail-product",
+    element: <DetailProduct />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: 'register-shop',
+    element: <RegisterShop />
+  }
+]);
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
